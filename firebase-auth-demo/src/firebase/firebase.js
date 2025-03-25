750217c75bd2ac58a21d52ec9869b67d8e02d54c
Pablo Mesén <pmesen2406@gmail.com>
2025-03-25 11:04:25 -0600

    Demo code subtle changes

--- firebase-auth-demo/src/firebase/firebase.js
+++ firebase-auth-demo/src/firebase/firebase.js
@@ -1,26 +1,3 @@
-/* // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getAuth } from "firebase/auth";
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyCgKCiclx8SWp58-6bpZUOn34F9zaGoZiA",
-  authDomain: "diseno-de-software-caso-2.firebaseapp.com",
-  projectId: "diseno-de-software-caso-2",
-  storageBucket: "diseno-de-software-caso-2.firebasestorage.app",
-  messagingSenderId: "824562010920",
-  appId: "1:824562010920:web:b83977a6f2a9d1e7a2de1d",
-  measurementId: "G-R5Y9R75Y35"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-const auth = getAuth(app)
-
-export {app, auth}; */
-
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
@@ -35,4 +12,4 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const auth = getAuth(app); 
